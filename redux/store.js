6cca6fb70f9a637aa8a300527e9fe089725f0cd3
seraph159 <56692720+seraph159@users.noreply.github.com>
2022-08-07 12:41:10 -0500

    redux persist implementation

--- redux/store.js
+++ redux/store.js
@@ -1,15 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../redux/features/user/userSlice'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import { combineReducers } from "redux"
 
-const store = configureStore({
-    reducer:{
-        user: userReducer
-    },
+const persistConfig = {
+  key: "user",
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
     middleware:  (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
     //
 })
 
-export default store
+export let persistor = persistStore(store)
