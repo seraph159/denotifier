699eab00fd58a5dd0bbe1ac0452d81d96513afdd
seraph159 <56692720+seraph159@users.noreply.github.com>
2022-08-06 19:04:59 -0500

    list component changes

--- components/list.component.jsx
+++ components/list.component.jsx
@@ -1,16 +1,51 @@
-
 import styles from '../styles/List.module.scss'
-
+import {updateDoc, arrayRemove} from 'firebase/firestore';
+import {onAuthStateChanged} from 'firebase/auth'
+import { auth, getUserProfileDocumentRef, createHnDocument } from '../firebase/firebase.utils';
 
 const List = ({listItem}) => {
 
+  let clonedArray = JSON.parse(JSON.stringify(listItem))
+  const handleDelete = (e) => {
+    let val = e.currentTarget.getAttribute('value')
+    let valNum = e.currentTarget.getAttribute('valueNum')
+    console.log(val)
+
+    const unsubscribeFromAuth = onAuthStateChanged(auth, async (userAuth) => {
+
+      const userRef = await getUserProfileDocumentRef(userAuth)
+      if (userAuth) {
+        if(val == "hNum"){
+        await updateDoc(userRef, {
+          hn:[]
+        });
+      } else{
+
+        var filtered = clonedArray.filter(prop => prop.name !== val)
+
+        clonedArray = filtered
+        console.log(filtered)
+  
+        await updateDoc(userRef, {
+        reddit: clonedArray
+        });
+
+      }
+   }})
+
+   unsubscribeFromAuth()
+  }
+
   return (
-    <div className={styles.dn__container}>
-        Your Current Digest:
+    <div >
         {listItem.map((e, idx) => (
+        <div className={styles.list__item} key={idx}>
         <li key={idx}>
-            {e.item}
-        </li>))}
+            {(e.name == "hNum") ? "HN (" + e.rNum + " Posts)" : e.name+ " (" + e.rNum + " Posts)"}
+        </li>
+        <a onClick={handleDelete} value={e.name} valueNum={e.rNum} className={styles.delete__btn}>Delete</a>
+        </div>))
+        }
     </div>
   )
 }
